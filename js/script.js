ae54d9a337514be94653a9237f1607ae4a6549b5
CarlosGo1994 <charls21ronald@gmail.com>
2020-02-11 19:28:08 -0600

    first commit about how to declare a variabe in JavaScript

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+//Variables
+//An varible is an information container
+
+
+var country = "Mexico";
+var continent = "America";
+var antiquity = "2020";
+var country_and_continent = country +' '+continent;
+
+country = "España";
+continent = "Europe";
+
+
+
+console.log(country,continent,antiquity);
+alert(country_and_continent);
